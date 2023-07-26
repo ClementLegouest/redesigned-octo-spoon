63be3fefd9fa53531e28f5c297de31e7b49d2d23
ClementLegouest <clement.legouest@gmail.com>
2023-07-26 14:42:29 +0200

    Update GenerateTableOfContent.ts

--- GenerateTableOfContent.ts
+++ GenerateTableOfContent.ts
@@ -20,8 +20,20 @@ function main(workbook: ExcelScript.Workbook) {
 
     workbook.getWorksheets().forEach( (worksheet) => {
         if(!SHEET_TO_AVOID_IN_SUMMARY.includes(worksheet.getName())) {
-            currentCell.setValue(worksheet.getName());
+            currentCell.setValue(hyperLinkToTab(worksheet.getName()));
+            /*
+            * At the moment, when inserting the formated link as the value of the cell, this is not really working and it shows the value #NAME?
+            * I have to manually update the cell doing <F2> then <Enter> to make the link works so this is not very handy
+            * I am actually looking for a solution to make this working
+            */
             currentCell = currentCell.getRowsBelow();
         }
     });
+}
+
+/**
+ * This will return a formated link to a tab
+ */
+function hyperLinkToTab(tabName: string) {
+    return "=LIEN_HYPERTEXTE(\"#${tabName}!A1\", \"${tabName}\")";
 }
