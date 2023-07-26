0a1b4a5ce344a56b450fd07bc26ba44cae119ee9
ClementLegouest <clement.legouest@gmail.com>
2023-07-26 15:11:13 +0200

    Update GenerateTableOfContent.ts

--- GenerateTableOfContent.ts
+++ GenerateTableOfContent.ts
@@ -5,12 +5,15 @@
 **
 **  This script will generate a list of all the sheets in the file in the SUMMARY_SHEET_NAME starting at the CELL_TO_START_SUMMARY
 **  Every sheet designated in the SHEET_TO_AVOID_IN_SUMMARY will be avoided
+**  Every sheet listed in the summary will have a home link to summary
 **
 */
 
 const SUMMARY_SHEET_NAME: string = "summary";
 const SHEET_TO_AVOID_IN_SUMMARY = ["summary"];
 const CELL_TO_START_SUMMARY = [0, 0];
+const CELL_LINK_TO_SUMMARY = [0, 1]
+const HOME_ICON = "🏠"
 
 function main(workbook: ExcelScript.Workbook) {
 
@@ -19,12 +22,11 @@ function main(workbook: ExcelScript.Workbook) {
 
     workbook.getWorksheets().forEach( (worksheet) => {
         if(!SHEET_TO_AVOID_IN_SUMMARY.includes(worksheet.getName())) {
-            currentCell.setValue(hyperLinkToTab(worksheet.getName()));
-            /*
-            * At the moment, when inserting the formated link as the value of the cell, this is not really working and it shows the value #NAME?
-            * I have to manually update the cell doing <F2> then <Enter> to make the link works so this is not very handy
-            * I am actually looking for a solution to make this working
-            */
+            worksheet
+                .getCell(CELL_LINK_TO_SUMMARY[0], CELL_LINK_TO_SUMMARY[1])
+                .setFormulaLocal(hyperLinkToTab("summary", HOME_ICON))
+            currentCell.setFormulaLocal(hyperLinkToTab(worksheet.getName()));
+
             currentCell = currentCell.getRowsBelow();
         }
     });
@@ -33,6 +35,6 @@ function main(workbook: ExcelScript.Workbook) {
 /**
  * This will return a formated link to a tab
  */
-function hyperLinkToTab(tabName: string) {
-    return "=LIEN_HYPERTEXTE(\"#${tabName}!A1\", \"${tabName}\")";
+function hyperLinkToTab(tabName: string, fancyName: string = tabName) {
+  return `=LIEN_HYPERTEXTE(\"#${tabName}!A1\", \"${fancyName}\")`;
 }
